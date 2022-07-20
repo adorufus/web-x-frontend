07871bcdf05798fa2b532f52e899aff194ab0aa0
Reingalrt <adorufus@gmail.com>
2022-07-20 22:11:57 +0700

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://103.163.139.152:8080/api/v1/',
+  baseUrl: 'http://localhost:8000/api/v1/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  baseUrl: "http://localhost:8000/api/v1/",
+  baseUrl: "https://103.163.139.152:8080/api/v1/",
 };
 
 /*
